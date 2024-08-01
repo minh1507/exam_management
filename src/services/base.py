c5f1ab91dfa65e2ebd3a109c63e9a77abb7275cc
Duong Duc Anh <duongdoican@gmail.com>
2024-08-01 12:15:58 +0700

    Feat: add token

--- src/services/base.py
+++ src/services/base.py
@@ -1,21 +1,22 @@
 import requests
 import os
 from dotenv import load_dotenv
-
+from ..common.static.global_c import Global
 load_dotenv()
 
 class BaseService(requests.Session):
     url = os.getenv("BASE_API_URL")
 
-    def __init__(self, token=None):
+    def __init__(self):
         super().__init__()
-        self.token = token
+        print(Global.token)
+        self.token = Global.token
 
     def request(self, method, url, **kwargs):
         if self.token is not None:
             if 'headers' not in kwargs:
                 kwargs['headers'] = {}
-            kwargs['headers']['Authorization'] = f'Bearer {self.token}'
+            kwargs['headers']['Authorization'] = f'{self.token}'
         return super().request(method, url, **kwargs)
     
     def wrap_url(self, path):
